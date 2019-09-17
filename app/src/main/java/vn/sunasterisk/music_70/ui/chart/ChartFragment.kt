e059b7dfd1e38cf33a9321a7a61a8e1f998290d0
kehaimat <nguyenchinhtho271@gmail.comqfdf>
2019-09-17 16:47:07 +0700

    Create Tab bar

---
+++ app/src/main/java/vn/sunasterisk/music_70/ui/chart/ChartFragment.kt
@@ -0,0 +1,32 @@
+package vn.sunasterisk.music_70.ui.chart
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import vn.sunasterisk.music_70.R
+import vn.sunasterisk.music_70.base.BaseFragment
+
+class ChartFragment : BaseFragment(){
+
+    override val getContentViewId = R.layout.fragment_chart
+
+    override fun initData(savedInstanceState: Bundle?) {
+    }
+
+    override fun initComponents() {
+    }
+
+    override fun registerListeners() {
+    }
+
+    override fun unregisterListeners() {
+    }
+
+    companion object {
+        fun newInstance() = ChartFragment()
+    }
+
+}
---
+++ app/src/main/java/vn/sunasterisk/music_70/ui/setting/SettingFragment.kt
@@ -0,0 +1,31 @@
+package vn.sunasterisk.music_70.ui.setting
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import vn.sunasterisk.music_70.R
+import vn.sunasterisk.music_70.base.BaseFragment
+
+class SettingFragment : BaseFragment(){
+
+    override val getContentViewId = R.layout.fragment_setting
+
+    override fun initData(savedInstanceState: Bundle?) {
+    }
+
+    override fun initComponents() {
+    }
+
+    override fun registerListeners() {
+    }
+
+    override fun unregisterListeners() {
+    }
+
+    companion object {
+        fun newInstance() = SettingFragment()
+    }
+}
