c7a9663e498e0c9bfbd5c08689c0182333c34dbe
kehaimat <nguyenchinhtho271@gmail.comqfdf>
2019-10-04 10:21:29 +0700

    Task#17532 Ui for Option Bottom Sheet

--- app/src/main/java/vn/sunasterisk/music_70/ui/nowplaying/BottomSheetFragment.kt
+++ app/src/main/java/vn/sunasterisk/music_70/ui/nowplaying/BottomSheetFragment.kt
@@ -1,9 +1,19 @@
 package vn.sunasterisk.music_70.ui.nowplaying
 
+<<<<<<< HEAD
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+=======
+import android.content.Intent
+import android.os.Bundle
+import android.provider.SyncStateContract
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+>>>>>>> Task#17532 Ui for Option Bottom Sheet
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import kotlinx.android.synthetic.main.bottom_sheet_option.*
 import vn.sunasterisk.music_70.R
@@ -11,6 +21,10 @@ import vn.sunasterisk.music_70.data.model.Track
 import vn.sunasterisk.music_70.data.remote.TrackAttributes
 import vn.sunasterisk.music_70.util.LoadImage
 
+<<<<<<< HEAD
+=======
+
+>>>>>>> Task#17532 Ui for Option Bottom Sheet
 class BottomSheetFragment : BottomSheetDialogFragment(), View.OnClickListener {
     private lateinit var track: Track
 
@@ -22,24 +36,36 @@ class BottomSheetFragment : BottomSheetDialogFragment(), View.OnClickListener {
 
             R.id.textAddToPlaylist -> dismiss()
 
-            R.id.textShare -> dismiss()
+            R.id.textShare -> {
+                val myShareIntent = Intent(Intent.ACTION_SEND)
+                myShareIntent.type = SHARE_TYPE
+                val value = String.format(
+                    SHARE_VALUE,
+                    track.title,
+                    track.username,
+                    track.streamUrl
+                )
+                myShareIntent.putExtra(Intent.EXTRA_TEXT, value)
+                startActivity(
+                    Intent.createChooser(
+                        myShareIntent,
+                        track.title
+                    )
+                )
+            }
 
             R.id.textInformation -> dismiss()
         }
     }
 
-    fun bindData() {
-        textSongName.text = track.title
-        textArtist.text = track.artist
+    fun bindData(){
+        textSongName.text=track.title
+        textArtist.text= track.artist
         track.artworkUrl?.let { LoadImage.loadImage(imageSong, it) }
 
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.bottom_sheet_option, container, false)
     }
 
@@ -69,7 +95,10 @@ class BottomSheetFragment : BottomSheetDialogFragment(), View.OnClickListener {
     }
 
     companion object {
+
         fun newInstance(track: Track): BottomSheetFragment {
+            const val SHARE_VALUE = "Track: %s\nArtits: %s\nLink: %s"
+            const val SHARE_TYPE = "text/plain"
             val args = Bundle().apply {
                 putParcelable(TrackAttributes.TRACK, track)
             }
@@ -79,3 +108,4 @@ class BottomSheetFragment : BottomSheetDialogFragment(), View.OnClickListener {
         }
     }
 }
+
